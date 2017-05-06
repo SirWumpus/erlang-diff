e77c2c460409666418d6d191d59fd05d0c2a5757
Anthony Howe <achowe@snert.com>
2017-05-06 06:38:33 -0400

    Look at playing with TypEr and Dialyzer with arr.erl; added -spec lines to arr.erl.

--- src/arr.erl
+++ src/arr.erl
@@ -4,20 +4,26 @@
 -module(arr).
 -export([new/3, get/2, set/3, size/1, lower/1, upper/1]).
 
+-spec new(non_neg_integer(),integer(),_) -> {'arr',integer(),array:array(_)}.
 new(Size, Zero, Defval) ->
 	{arr, Zero, array:new(Size, [{default, Defval}, {fixed, true}])}.
 
+-spec get(integer(),{'arr',integer(),array:array(_)}) -> any().
 get(Index, {arr, Zero, Arr}) ->
 	array:get(Zero+Index, Arr).
 
+-spec set(integer(),_,{'arr',integer(),array:array(_)}) -> {'arr',integer(),array:array(_)}.
 set(Index, Value, {arr, Zero, Arr}) ->
 	{arr, Zero, array:set(Zero+Index, Value, Arr)}.
 
+-spec size({'arr',_,array:array(_)}) -> non_neg_integer().
 size({arr, _Zero, Arr}) ->
 	array:size(Arr).
 
+-spec lower({'arr',integer(),_}) -> integer().
 lower({arr, Zero, _Arr}) ->
 	-Zero.
 
+-spec upper({'arr',integer(),array:array(_)}) -> integer().
 upper({arr, Zero, Arr}) ->
 	array:size(Arr) - Zero - 1.
